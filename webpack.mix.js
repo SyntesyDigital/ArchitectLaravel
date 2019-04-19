44afd2007facc092a66e9907f07a1c3d9c419882
SyntesyDigital <ndelcast.dev@gmail.com>
2019-04-19 21:24:50 +0200

    Reset to default composer.json

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,42 @@ let mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
+ | ARCHITECT BUILD ASSETS
+ |--------------------------------------------------------------------------
+ |
+ |  TODO : Perhaps we can make a npm plugin ?
+ |  Read architect modules webpack.build.js and build assets
+ |
+ */
+const ArchitectBuilds = [];
+
+// !! DON'T REMOVE THIS !!
+// {ARCHITECT:BUILD}
+// ArchitectBuilds.push(require('./Modules/Architect/webpack.build.js'));
+//
+
+ArchitectBuilds.map(function(build){
+
+    var _build = build.build;
+
+    // ReactJS
+    if(_build.react !== undefined) {
+        mix.react(_build.react.src, _build.react.path);
+    }
+
+    // SASS
+    if(_build.sass !== undefined) {
+        mix.sass(_build.sass.src, _build.sass.path);
+    }
+
+    // Scripts
+    if(_build.scripts !== undefined) {
+        mix.scripts(_build.scripts.src, _build.scripts.path);
+    }
+});
+
+/*
+ |--------------------------------------------------------------------------
  | Mix Asset Management
  |--------------------------------------------------------------------------
  |
@@ -11,5 +47,5 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
+mix.react('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
